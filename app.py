2954a0c10ed62436e815e33adc488878ae9b38b3
Shinde Aditya <heyshinde@icloud.com>
2024-04-19 13:41:47 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,45 @@
+import os
+import shutil
+import streamlit as st
+from PIL import Image
+from ultralytics import YOLO
+
+# Load YOLO model
+model = YOLO('/Users/shinde/Documents/Woxsen/Sem VI/Minor 2/Yolov8/runs/detect/train2/weights/best.pt')
+
+# Function to track objects in a video
+def track_objects(video_path, output_dir):
+    result = model.track(video_path, save=True, save_dir=output_dir)
+    # Find the latest track folder
+    latest_track_folder = max([os.path.join(output_dir, d) for d in os.listdir(output_dir)], key=os.path.getmtime)
+    tracked_video_path = os.path.join(latest_track_folder, "temp_video.mp4")
+    print(latest_track_folder)
+    return tracked_video_path
+
+# Streamlit UI
+st.title('Object Tracking')
+
+# Video tracking section
+st.header('Video Tracking')
+video_file = st.file_uploader("Upload a video", type=['mp4'])
+
+if video_file is not None:
+    st.video(video_file)
+    if st.button('Track Objects'):
+        with st.spinner('Tracking objects...'):
+            # Save uploaded video to a temporary location
+            video_path = "temp_video.mp4"
+            output_dir = "runs/detect"
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+            with open(video_path, "wb") as f:
+                f.write(video_file.read())
+            tracked_video_path = track_objects(video_path, output_dir)
+            if tracked_video_path:
+                st.video(tracked_video_path)
+            else:
+                st.error("Error occurred while tracking objects.")
+
+# Cleanup temporary files
+if os.path.exists("temp_video.mp4"):
+    os.remove("temp_video.mp4")
